a573c3e4b3e56104b8009b1b9f45f8cc7afef51c
BasilZap <zapr9@yandex.ru>
2023-11-25 00:40:07 +0300

    Создано отображение моделей в панели администратора, добавлена команда для создания администратора.

--- selfedu/admin.py
+++ selfedu/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from selfedu.models import Chapter, Material, TestQuestion, TestAnswer
+
+
+@admin.register(Chapter)
+class ChapterAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'image', 'last_update')
+
+
+@admin.register(Material)
+class MaterialAdmin(admin.ModelAdmin):
+    list_display = ('chapter', 'name', 'image', 'video', 'description', 'last_update')
+
+
+@admin.register(TestQuestion)
+class TestQuestionAdmin(admin.ModelAdmin):
+    list_display = ('material', 'question', 'hint')
+
+
+@admin.register(TestAnswer)
+class TestAnswerAdmin(admin.ModelAdmin):
+    list_display = ('question', 'answer', 'is_true')
