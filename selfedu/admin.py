aee6ae4e45949e59378d466d52f3655f7f291303
BasilZap <zapr9@yandex.ru>
2023-12-08 01:19:13 +0300

    В панель администратора (в admin.py) прописаны отображаемые значения моделей и фильтры, добавлен класс UserTestCompleteAdmin.

--- selfedu/admin.py
+++ selfedu/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from selfedu.models import Chapter, Material, TestQuestion, TestAnswer
+from selfedu.models import Chapter, Material, TestQuestion, TestAnswer, UserTestComplete
 
 
 @admin.register(Chapter)
@@ -10,14 +10,23 @@ class ChapterAdmin(admin.ModelAdmin):
 
 @admin.register(Material)
 class MaterialAdmin(admin.ModelAdmin):
-    list_display = ('chapter', 'name', 'image', 'video', 'description', 'last_update')
+    list_display = ('id', 'chapter', 'name', 'image', 'video', 'description', 'last_update')
+    list_filter = ('chapter',)
 
 
 @admin.register(TestQuestion)
 class TestQuestionAdmin(admin.ModelAdmin):
-    list_display = ('material', 'question', 'hint')
+    list_display = ('id', 'material', 'question', 'hint')
+    list_filter = ('material',)
 
 
 @admin.register(TestAnswer)
 class TestAnswerAdmin(admin.ModelAdmin):
-    list_display = ('question', 'answer', 'is_true')
+    list_display = ('id', 'question', 'answer', 'is_true')
+    list_filter = ('question',)
+
+
+@admin.register(UserTestComplete)
+class UserTestCompleteAdmin(admin.ModelAdmin):
+    list_display = ('user', 'question', 'is_done')
+    list_filter = ('user',)
